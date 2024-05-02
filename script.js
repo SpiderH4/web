ecc42c7db2dff766840b047bfe28a15a0cfe68eb
SpiderH4 <167268634+SpiderH4@users.noreply.github.com>
2024-05-01 20:13:51 -0600

    script.js

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ const downloadButtonLinux = document.getElementById('download-button-linux');
 
 downloadButtonWindows.addEventListener('click', () => {
   const link = document.createElement('a');
-  link.href = 'https://github.com/Spiderh2/Spider-Launcher/releases/download/1.0.2/spider-client-win-x64.exe';
+  link.href = 'https://github.com/Spiderh2/Spider-Launcher/releases/download/1.0.4/spider-client-win-x64.exe';
   link.download = 'spider-client-win-x64.exe';
   link.click();
 });
 
 downloadButtonMacos.addEventListener('click', () => {
   const link = document.createElement('a');
-  link.href = 'https://github.com/Spiderh2/Spider-Launcher/releases/download/1.0.2/spider-client-mac-universal.dmg';
+  link.href = 'https://github.com/Spiderh2/Spider-Launcher/releases/download/1.0.4/spider-client-mac-universal.zip';
   link.download = 'spider-client-mac-universal.dmg';
   link.click();
 });
 
 downloadButtonLinux.addEventListener('click', () => {
   const link = document.createElement('a');
-  link.href = 'https://github.com/Spiderh2/Spider-Launcher/releases/download/1.0.2/spider-client-linux-x86_64.AppImage';
+  link.href = 'https://github.com/Spiderh2/Spider-Launcher/releases/download/1.0.4/spider-client-linux-x86_64.AppImage';
   link.download = 'spider-client-linux-x86_64.AppImage';
   link.click();
 });
